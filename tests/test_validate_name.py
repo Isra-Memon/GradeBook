ecf66893b95a2c42fca6e3a9b221f467509a14e0
Isra Memon <isramemon0786@gmail.com>
2026-09-15 15:02:31 +0500

    Update test_validate_name.py
    Added a test to validate name length boundaries.

--- tests/test_validate_name.py
+++ tests/test_validate_name.py
@@ -19,3 +19,13 @@ def test_validate_name_over_length():
 def test_validate_name_contains_digits_symbols():
     with pytest.raises(ValueError):
         validate_name("Ali123!")
+
+
+@pytest.mark.parametrize("length", [0, 1, 49, 50, 51])
+def test_validate_name_length_boundaries(length):
+    name = "A" * length
+    if 1 <= length <= 50:
+        assert validate_name(name) is True
+    else:
+        with pytest.raises(ValueError):
+            validate_name(name)
