1284506a04dc179bac35ed82eb8986222c264557
Isra Memon <isramemon0786@gmail.com>
2026-09-22 15:02:05 +0500

    Refactor test_roster.py to use empty_roster fixture

--- tests/test_roster.py
+++ tests/test_roster.py
@@ -3,30 +3,28 @@ from gradebook import Student, Roster
 
 
 @pytest.mark.parametrize("num_scores", [0, 3, 8])
-def test_roster_add_student_score_count(num_scores):
-    roster = Roster()
+def test_roster_add_student_score_count(empty_roster, num_scores):
     student = Student("Test Student", 999)
     for i in range(num_scores):
         student.add_score(70)
 
     if 1 <= num_scores <= 6:
-        roster.add_student(student)
-        assert student in roster.students
+        empty_roster.add_student(student)
+        assert student in empty_roster.students
     else:
         with pytest.raises(ValueError):
-            roster.add_student(student)
+            empty_roster.add_student(student)
 
 
 @pytest.mark.parametrize("num_scores", [0, 1, 2, 5, 6, 7])
-def test_roster_score_count_boundaries(num_scores):
-    roster = Roster()
+def test_roster_score_count_boundaries(empty_roster, num_scores):
     student = Student("Boundary Student", 500)
     for i in range(num_scores):
         student.add_score(70)
 
     if 1 <= num_scores <= 6:
-        roster.add_student(student)
-        assert student in roster.students
+        empty_roster.add_student(student)
+        assert student in empty_roster.students
     else:
         with pytest.raises(ValueError):
-            roster.add_student(student)
+            empty_roster.add_student(student)
